996fcf4533d341b35923f26109bcaa45ca723cca
th2nhb1nh <thanhbinh.pham610@gmail.com>
2025-05-16 15:40:42 +0700

    log more patterns to the console

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-export function messageLogger(payload){
-  console.log(`Logging message: ${payload.message}`);
+export function messageLogger(payload) {
+  console.log(`Message: ${payload.message}`);
+  console.log(`Payload: ${JSON.stringify(payload)}`);
+  return `Successfully logged payload: ${payload.message}`
 }
